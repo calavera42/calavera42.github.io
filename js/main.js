2520c0051f528ca93a988c83d0e39fc34035a478
calavera <gregdestemione@gmail.com>
2021-04-04 14:44:23 -0300

    JS Standalonne conversion

---
+++ js/main.js
@@ -0,0 +1,11 @@
+$(function(){
+    var prev;
+    $("a").hover(
+        function(){
+            prev = $(this).text();
+            $(this).text(`[ ${prev} ]`);
+        },
+        function(){
+            $(this).text(`${prev}`);
+    })
+})
