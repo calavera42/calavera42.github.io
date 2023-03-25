0dd87f0e63c8925e82894abd0addd47c444ab168
calavera <34943891+calavera42@users.noreply.github.com>
2023-03-24 22:28:26 -0300

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,10 @@
+$(function() {
+    if (RandomIntFromInterval(0, 100) != 70)
+    {
+        $("footer").hide();
+    }
+})
+
+function RandomIntFromInterval(min, max) {
+    return Math.floor(Math.random() * (max - min + 1) + min)
+}
