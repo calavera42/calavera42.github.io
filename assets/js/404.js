b24e35cfa9d2dc666f8727d218e0573f2f12a4c8
calavera42 <gregdestemione@gmail.com>
2025-04-12 19:04:09 -0300

    Contagem regressiva adicionada na página 404

---
+++ assets/js/404.js
@@ -0,0 +1,29 @@
+function iniciarContagem(elemento, duracao, audioInt) {
+    let tempoRestante = duracao;
+    elemento.innerHTML = `T-${String(tempoRestante).padStart(2, '0')}S`;
+
+    const intervalo = setInterval(() => {
+        tempoRestante--;
+        elemento.innerHTML = `T-${String(tempoRestante).padStart(2, '0')}S`;
+
+        if (tempoRestante <= 0) {
+            clearInterval(intervalo);
+            clearInterval(audioInt);
+            elemento.innerHTML = "T-00S";
+            window.location = "https://calavera.xyz/"
+        }
+    }, 1000);
+    console.log('a')
+}
+
+document.addEventListener("DOMContentLoaded", () =>{
+    const elementos = document.querySelectorAll('.cntdwn');
+    const audio = new Audio("https://calavera.xyz/assets/audio/beep.mp3");
+
+    audio.volume = .15;
+
+    const audioInt = setInterval(() => {
+        audio.play();
+    }, 1000);
+    elementos.forEach(el => iniciarContagem(el, 15, audioInt));
+})
