a089a1ba930c6eaa18b4871661ee06f5226e7e30
calavera42 <gregdestemione@gmail.com>
2025-04-12 21:21:01 -0300

    "Tela de carregamento" adicionada

---
+++ assets/js/loader.js
@@ -0,0 +1,20 @@
+const anim = "/-\\|"
+
+document.addEventListener("DOMContentLoaded", () => {
+    const loader = document.getElementById("loader");
+    const mainpg = document.getElementById("main")
+    const phrase = document.getElementById("phrase");
+
+    let counter = 0;
+
+    const ticker = setInterval(() => {
+        phrase.textContent = anim[(counter++) % anim.length];
+    }, 100);
+
+    document.fonts.ready.then(() => {
+        mainpg.style.visibility = "visible";
+        loader.remove();
+
+        clearInterval(ticker);
+    });
+})
