6e6476785168a6adbcaf22244bc2cf20d087dfb5
calavera42 <gregdestemione@gmail.com>
2025-08-29 17:29:13 -0300

    script

--- assets/js/loader.js
+++ assets/js/loader.js
@@ -1,4 +1,18 @@
 const anim = "/-\\|"
+let stage = 0;
+
+function done() 
+{
+    stage++;
+
+    if(stage == 2)
+    {
+        mainpg.style.visibility = "visible";
+        loader.remove();
+        
+        clearInterval(ticker);
+    }
+}
 
 document.addEventListener("DOMContentLoaded", () => {
     const loader = document.getElementById("loader");
@@ -11,10 +25,13 @@ document.addEventListener("DOMContentLoaded", () => {
         phrase.textContent = `Baixando recursos ${anim[(counter++) % anim.length]}`;
     }, 100);
 
+    if (img.complete) {
+        done()
+    } else {
+        img.addEventListener('load', done)
+    }
+
     document.fonts.ready.then(() => {
-        mainpg.style.visibility = "visible";
-        loader.remove();
-        
-        clearInterval(ticker);
+        done()
     });
 })
